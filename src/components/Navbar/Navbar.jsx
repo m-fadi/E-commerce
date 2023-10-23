c78802d4d6ea5f8bbd4d3acd469f9af0d57f6490
fadi <fadi.m.marouf@gmail.com>
2023-10-23 20:32:53 +0200

    navbar menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,10 +2,21 @@ import React from "react";
 import "./Navbar.css";
 import logo from "../assest/images/logo.png"
 import cart_icon from "../assest/images/cart_icon.png"
+
 function Navbar() {
     return <div className="navbar">
         <div className="nav-logo">
-            <img src={logo} alt="" />
+            <img src={logo} alt="logo" />
+        </div>
+        <ul className="nav-menu">
+            <li>Shop</li>
+            <li>Men</li>
+            <li>Women</li>
+            <li>Kids</li>
+        </ul>
+        <div className="nav-login-cart">
+            <button>Login</button>
+            <img src={cart_icon} alt=" cart icon"  />
         </div>
     </div>;
 }
