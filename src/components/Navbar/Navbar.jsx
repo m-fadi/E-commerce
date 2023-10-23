7df7125e554136bcc38d84e21e120877f1a5295c
fadi <fadi.m.marouf@gmail.com>
2023-10-23 23:26:59 +0200

    ad count

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -31,7 +31,7 @@ function Navbar() {
             <div className="nav-login-cart">
                 <button>Login</button>
                 <img src={cart_icon} alt=" cart icon" />
-                <div className="nav-cart-count">1</div>
+                <div className="nav-cart-count">0</div>
             </div>
         </div>
     );
