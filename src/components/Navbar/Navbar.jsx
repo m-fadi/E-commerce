ee946c0b41d79645e42b174cc9f15849e30a30d8
fadi <fadi.m.marouf@gmail.com>
2023-10-23 23:14:31 +0200

    Navbar cart

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,39 @@
 import React from "react";
 import "./Navbar.css";
-import logo from "../assest/images/logo.png"
-import cart_icon from "../assest/images/cart_icon.png"
+import logo from "../assest/images/logo.png";
+import cart_icon from "../assest/images/cart_icon.png";
 
 function Navbar() {
-    return <div className="navbar">
-        <div className="nav-logo">
-            <img src={logo} alt="logo" />
+    return (
+        <div className="navbar">
+            <div className="nav-logo">
+                <img src={logo} alt="logo" />
+                <p>SHOPER</p>
+            </div>
+            <ul className="nav-menu">
+                <li>
+                    Shop
+                    <hr/>
+                </li>
+                <li>
+                    Men
+                    
+                </li>
+                <li>
+                    Women 
+                </li>
+                <li>
+                    Kids
+                    
+                </li>
+            </ul>
+            <div className="nav-login-cart">
+                <button>Login</button>
+                <img src={cart_icon} alt=" cart icon" />
+                <div className="nav-cart-count">0</div>
+            </div>
         </div>
-        <ul className="nav-menu">
-            <li>Shop</li>
-            <li>Men</li>
-            <li>Women</li>
-            <li>Kids</li>
-        </ul>
-        <div className="nav-login-cart">
-            <button>Login</button>
-            <img src={cart_icon} alt=" cart icon"  />
-        </div>
-    </div>;
+    );
 }
 
 export default Navbar;
