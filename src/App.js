055ea244524db9b45a5ddac2c48df45e3dbdac75
fadi <fadi.m.marouf@gmail.com>
2023-10-24 13:40:21 +0200

    import shop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Shop from './pages/Shop';
 
 function App() {
   return (
     <div >
      <Navbar/>
+     <Shop/>
     </div>
   );
 }
