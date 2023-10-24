8e01e53de4ef97c89571c9d51bff78f54eaa30bc
fadi <fadi.m.marouf@gmail.com>
2023-10-24 13:54:44 +0200

    shop

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,18 +1,9 @@
 import all_products from '../components/assest/images/all_product'
 
 function Shop() {
-let product=all_products.map(product=>{
-    return`
-    <div class="product"> 
-    <div className="name"> hhhhhhhhhh${product.name}</div>
 
-    </div>
-    ` 
-})
 
-  return (
-    <div>{product.name}</div>
-  )
+  return <div>LoginSignUp</div>;
 }
 
 export default Shop
