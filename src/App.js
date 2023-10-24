7cf53c5f4e66f10a671baa12720b3ba34baeb7d4
fadi <fadi.m.marouf@gmail.com>
2023-10-24 16:54:13 +0200

    routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Shop from "./pages/Shop";
-import Women from "./pages/Women";
-import Men from "./pages/Men";
-import Kids from "./pages/Kids";
+import LoginSignUp from "./pages/LoginSignUp";
+import Cart from "./pages/Cart";
+import Product from "./pages/Product";
+import ShopCategory from "./pages/ShopCategory";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
     return (
@@ -13,9 +13,23 @@ function App() {
                 <Navbar />
                 <Routes>
                     <Route path="/" element={Shop} />
-                    <Route path="/men" element={Men} />
-                    <Route path="/women" element={Women} />
-                    <Route path="/kids" element={Kids} />
+                    <Route
+                        path="/men"
+                        element={<ShopCategory category="men" />}
+                    />
+                    <Route
+                        path="/women"
+                        element={<ShopCategory category="women" />}
+                    />
+                    <Route
+                        path="/kids"
+                        element={<ShopCategory category="kids" />}
+                    />
+                    <Route path="product" element={<Product />}>
+                        <Route path=":productId" element={<Product />} />
+                    </Route>
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/login" element={<LoginSignUp />} />
                 </Routes>
             </BrowserRouter>
         </div>
