732ad8c5bfb329ef4667dceb0dc2361ba1baa4d5
fadi <fadi.m.marouf@gmail.com>
2023-10-24 14:25:19 +0200

    routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import Shop from './pages/Shop';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Shop from "./pages/Shop";
+import Women from "./pages/Women";
+import Men from "./pages/Men";
+import Kids from "./pages/Kids";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
-  return (
-    <div >
-     <Navbar/>
-     <Shop/>
-    </div>
-  );
+    return (
+        <div>
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={Shop} />
+                    <Route path="/men" element={Men} />
+                    <Route path="/women" element={Women} />
+                    <Route path="/kids" element={Kids} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
