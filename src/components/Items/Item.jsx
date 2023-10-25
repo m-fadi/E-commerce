3ce05db773c315ca696aea55082c2a84b11a18a5
fadi <fadi.m.marouf@gmail.com>
2023-10-25 18:52:34 +0200

    modify price

--- src/components/Items/Item.jsx
+++ src/components/Items/Item.jsx
@@ -8,8 +8,8 @@ function Item(props) {
             <img src={item.image} alt="" />
             <p> {item.name}</p>
             <div className="item-price">
-                <div className="item-price-old">{item.old_price}</div>
-                <div className="item-price-new">{item.new_price}</div>
+                <div className="item-price-old">{item.old_price} $</div>
+                <div className="item-price-new">{item.new_price} $</div>
             </div>
         </div>
     );
