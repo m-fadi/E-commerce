9f784bca7290091b34f2cbc5efacd8a3cde48097
fadi <fadi.m.marouf@gmail.com>
2023-10-25 19:06:16 +0200

    offer component

---
+++ src/components/Offers/Offer.jsx
@@ -0,0 +1,20 @@
+import './Offer.css'
+import React from 'react'
+
+function Offer() {
+  return (
+    <div className='offer'>
+        <div className="left-baner">
+            <h1> Exclusive</h1>
+            <h1> Offers for You</h1>
+            <p>ONLY ON BEST SELLERS PRODUCTS</p>
+            <div className="check-now">Check Now</div>
+        </div>
+        <div className="right-banner">
+            <img src="" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Offer
