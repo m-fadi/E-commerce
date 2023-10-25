57878415dadb04e641e84b9c194c42b6e1f53a32
fadi <fadi.m.marouf@gmail.com>
2023-10-25 17:14:17 +0200

    add Item props

--- src/components/Items/Item.jsx
+++ src/components/Items/Item.jsx
@@ -1,16 +1,18 @@
-import './Item.css'
+import "./Item.css";
 
 function Item(props) {
-  return (
-      <div className="item">
-          <img src={props.image} alt="" />
-          <p> {props.name}</p>
-          <div className="item-price">
-              <div className="item-price-old">{props.old_price}</div>
-              <div className="item-price-new">{props.new_price}</div>
-          </div>
-      </div>
-  );
+    let { item } = props;
+    
+    return (
+        <div className="item">
+            <img src={item.image} alt="" />
+            <p> {item.name}</p>
+            <div className="item-price">
+                <div className="item-price-old">{item.old_price}</div>
+                <div className="item-price-new">{item.new_price}</div>
+            </div>
+        </div>
+    );
 }
 
-export default Item
+export default Item;
