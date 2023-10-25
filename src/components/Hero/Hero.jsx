81cfcfc148fe676eb21114c65c37afdbf1076cf5
fadi <fadi.m.marouf@gmail.com>
2023-10-25 15:04:10 +0200

    Hero left

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,33 @@
-import './Hero.css'
-function Hero() {
-  return (
-    <div className='hero'>
+import "./Hero.css";
+import hand_icon from "../assest/images/hand_icon.png"
+import hero_image from "../assest/images/hero_image.png"
+import arrow_icon from "../assest/images/arrow.png";
 
-        
-    </div>
-  )
+function Hero() {
+    return (
+        <div className="hero">
+            <div className="hero-left">
+                <h2> New Arrivals only</h2>
+                <div>
+                    <div className="hero-hand-icon">
+                        <p> new</p>
+                        <img src={hand_icon} alt=" hand icon" />
+                    </div>
+                    <p> collections</p>
+                    <p> for everyone</p>
+                    <div className="hero-latest-btn">
+                        <div>Latest Collection</div>
+                        <img src={arrow_icon} alt="" className="src" />
+                    </div>
+                </div>
+            </div>
+            <div className="hero-right">
+                <div className="hero-img">
+                    <img src={hero_image} alt=" " />
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default Hero
+export default Hero;
