4507af195bf8efaa7c0bcdf6a8f2ad10c4f2f0dc
fadi <fadi.m.marouf@gmail.com>
2023-10-25 16:58:04 +0200

    add Popular componat to home

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,10 +1,12 @@
 import Hero from '../components/Hero/Hero'
+import Popular from '../components/popular_items/Popular';
 
 function Shop() {
 console.log(Hero)
 
   return <div>
     <Hero/>
+    <Popular/>
   </div>;
 }
 
