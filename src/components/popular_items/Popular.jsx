8e7272ecd2e3c80720fa3f9b96940e4d434aec29
fadi <fadi.m.marouf@gmail.com>
2023-10-25 17:14:27 +0200

    map popular datra

--- src/components/popular_items/Popular.jsx
+++ src/components/popular_items/Popular.jsx
@@ -7,13 +7,14 @@ function Popular() {
     let popularItems = popular_items.map((item) => {
         return <Item key={item.id} id={item.id} item={item} />;
     });
-    return <div className="popular">
-        <h1> Popular Women's items</h1>
-        <hr/>
-        <div className="popular-item">
-            {popularItems}
+
+    return (
+        <div className="popular">
+            <h1> Popular Women's items</h1>
+            <hr />
+            <div className="popular-item">{popularItems}</div>
         </div>
-    </div>;
+    );
 }
 
 export default Popular;
