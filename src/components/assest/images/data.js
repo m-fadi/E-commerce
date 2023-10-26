93a03f6ac44c3f036cebca6178cfbf4c164a092a
fadi <fadi.m.marouf@gmail.com>
2023-10-26 12:27:49 +0200

    removing data

--- src/components/assest/images/data.js
+++ src/components/assest/images/data.js
@@ -32,41 +32,7 @@ let data_product = [
         new_price: 100.0,
         old_price: 150.0,
     },
-    {
-        id: 5,
-        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
-        image: p4_img,
-        new_price: 100.0,
-        old_price: 150.0,
-    },
-    {
-        id: 6,
-        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
-        image: p1_img,
-        new_price: 50.0,
-        old_price: 80.5,
-    },
-    {
-        id: 7,
-        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
-        image: p2_img,
-        new_price: 85.0,
-        old_price: 120.5,
-    },
-    {
-        id: 5,
-        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
-        image: p4_img,
-        new_price: 100.0,
-        old_price: 150.0,
-    },
-    {
-        id: 6,
-        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
-        image: p1_img,
-        new_price: 50.0,
-        old_price: 80.5,
-    },
+   
 ];
 
 export default data_product;
