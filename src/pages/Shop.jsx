9ed857a436ab0fea624b1d5e5980127492e86fd4
fadi <fadi.m.marouf@gmail.com>
2023-10-26 13:21:21 +0200

    add newsLetter comp to shop

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -2,15 +2,17 @@ import Hero from '../components/Hero/Hero'
 import Popular from '../components/popular_items/Popular';
 import Offer from '../components/Offers/Offer';
 import NewCollections from '../components/New_collections/NewCollections';
+import NewsLetter from '../components/NewsLetter/NewsLetter';
 
 function Shop() {
 console.log(Hero)
 
-  return <div className='container'>
+  return <div >
     <Hero/>
     <Popular/>
     <Offer/>
     <NewCollections/>
+    <NewsLetter/>
   </div>;
 }
 
