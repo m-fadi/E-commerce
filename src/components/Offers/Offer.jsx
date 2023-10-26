d01462621b9a78dc14ba40a4c1e8b48eca3ce44a
fadi <fadi.m.marouf@gmail.com>
2023-10-26 10:56:32 +0200

    offen btn

--- src/components/Offers/Offer.jsx
+++ src/components/Offers/Offer.jsx
@@ -1,20 +1,24 @@
 import './Offer.css'
 import exclusive_image from "../assest/images/exclusive_image.png" 
+import arrow_icon from "../assest/images/arrow.png";
 
 function Offer() {
   return (
-    <div className='offer'>
-        <div className="left-baner">
-            <h1> Exclusive</h1>
-            <h1> Offers for You</h1>
-            <p>ONLY ON BEST SELLERS PRODUCTS</p>
-            <div className="check-now">Check Now</div>
-        </div>
-        <div className="right-banner">
-            <img src={exclusive_image} alt="" />
-        </div>
-    </div>
-  )
+      <div className="offer">
+          <div className="left-banner">
+              <h1> Exclusive</h1>
+              <h1> Offers for You</h1>
+              <p>ONLY ON BEST SELLERS PRODUCTS</p>
+              <div className="check-now-btn">
+                  <div>Check Now</div>
+                  <img src={arrow_icon} alt="" />
+              </div>
+          </div>
+          <div className="right-banner">
+              <img src={exclusive_image} alt="" />
+          </div>
+      </div>
+  );
 }
 
 export default Offer
