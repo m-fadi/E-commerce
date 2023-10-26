8dccff74345f051eb42bc79ee4b4e09091684bbc
fadi <fadi.m.marouf@gmail.com>
2023-10-26 11:48:20 +0200

    component new collections

---
+++ src/components/New_collections/NewCollections.jsx
@@ -0,0 +1,17 @@
+import new_collections from '../assest/images/new_collections'
+import "./NewCollections.css"
+import Item from "../Items/Item"
+function NewCollections() {
+    let newCollections = new_collections.map(item=>{
+        return <div> <Item key={item.id} item={item}/></div>
+    })
+  return (
+      <div class="new-collections">
+          <h1> NEW COLLECTIONS</h1>
+          <hr />
+          <div className="new-collections-item"> {newCollections}</div>
+      </div>
+  );
+}
+
+export default NewCollections
