4aece83aecc9c4ca049bdffc43c366c77cc2f000
fadi <fadi.m.marouf@gmail.com>
2023-10-28 00:02:44 +0200

    maping filtered items

--- src/pages/ShopCategory.jsx
+++ src/pages/ShopCategory.jsx
@@ -3,6 +3,7 @@ import "./Css/ShopCategory.css";
 import { ShopContext } from "../Context/ShopContext";
 import all_products from "../components/assest/images/all_product";
 import Item from "../components/Items/Item";
+
 function ShopCategory(props) {
     const { specialOffer, category } = props;
     const filteredItems = all_products
@@ -11,16 +12,18 @@ function ShopCategory(props) {
             else return null;
         })
         .map((item) => <Item item={item} />);
-    console.log({ filteredItems });
+    
     return (
-        <div className="container">
-            <div>
+        <div className="shop-container">
+            <div className="shop-offer-banner">
                 <img src={specialOffer} alt="" />
             </div>
-            <p>
+            <div className="shop-count">
                 {" "}
-                Showing 1 - {filteredItems.length} of {all_products.length}{" "}
-            </p>
+                <p>
+                    Showing 1 - {filteredItems.length} of {filteredItems.length}{" "}
+                </p>
+            </div>
             <div className="filtered">
                 <div className="filtered-items"> {filteredItems} </div>
             </div>
