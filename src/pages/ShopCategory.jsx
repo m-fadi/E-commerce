7c4e0feca0f3d210c24e826983a27aad4f5009dd
fadi <fadi.m.marouf@gmail.com>
2023-10-27 22:50:47 +0200

    filtering items

--- src/pages/ShopCategory.jsx
+++ src/pages/ShopCategory.jsx
@@ -1,22 +1,29 @@
-import React, { useContext } from 'react'
-import  {ShopContext} from '../Context/ShopContext'
+import React, { useContext } from "react";
+import { ShopContext } from "../Context/ShopContext";
 import all_products from "../components/assest/images/all_product";
-
+import Item from "../components/Items/Item";
 function ShopCategory(props) {
- // const { all_products } = useContext(ShopContext);
-  const {specialOffer, category} = props
-  const filteredItems=all_products.filter(item=>{
-if (item.category===category ) return item
-  })
-  console.log({filteredItems})
-  return (
-      <div>
-          <div>
-              <img src={specialOffer} alt="" />
-          </div>
-          <p> Showing 1 - {filteredItems.length} of {all_products.length} </p>
-      </div>
-  );
+    // const { all_products } = useContext(ShopContext);
+    const { specialOffer, category } = props;
+    const filteredItems = all_products
+        .filter((item) => {
+            if (item.category === category) return item 
+            else return null
+        })
+        .map((item) => <Item item={item} />);
+    console.log({ filteredItems });
+    return (
+        <div>
+            <div>
+                <img src={specialOffer} alt="" />
+            </div>
+            <p>
+                {" "}
+                Showing 1 - {filteredItems.length} of {all_products.length}{" "}
+            </p>
+            <div className="filtered-items"> {filteredItems} </div>
+        </div>
+    );
 }
 
-export default ShopCategory
+export default ShopCategory;
