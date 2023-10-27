43e4f00358d9dcc833366dc63ff2429c5d8b0ab5
fadi <fadi.m.marouf@gmail.com>
2023-10-27 22:35:45 +0200

    shopContext

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -5,6 +5,7 @@ import all_products from'../components/assest/images/all_product'
 export const ShopContext = createContext(null)
 const ShopContextProvider=(props)=>{
     const contextValue= { all_products}
+    console.log({all_products})
     return (
         <ShopContext.Provider value={contextValue}>
             {props.children}
