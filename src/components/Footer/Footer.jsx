866e53a7afbef7184987bc57c233792311a582f2
fadi <fadi.m.marouf@gmail.com>
2023-10-27 09:26:37 +0200

    clean

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import logo from "../assest/images/logo_big.png";
+import "./Footer.css";
+import logo from "../assest/images/logo.png";
 import whatsapp_icon from "../assest/images/whatsapp_icon.png";
-import  pinter_icon from "../assest/images/pintester_icon.png";
-import  instagram_icon from "../assest/images/instagram_icon.png";
+import pinter_icon from "../assest/images/pintester_icon.png";
+import instagram_icon from "../assest/images/instagram_icon.png";
 function Footer() {
     return (
         <footer>
-            <dir>
-                <img src={logo} alt="" />
-                <h1>SHOPER</h1>
+            <footer>
+                <div className="logo">
+                    <img src={logo} alt="" />
+                    <h1>SHOPER</h1>
+                </div>
                 <ul className="information">
                     <li>Company</li>
 
@@ -23,7 +26,7 @@ function Footer() {
                     <img src={pinter_icon} alt="" />
                 </div>
                 <p> copyright @2023 - All Rights Reserved</p>
-            </dir>
+            </footer>
         </footer>
     );
 }
