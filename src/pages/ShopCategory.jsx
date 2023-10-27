85bf71c19bee39ef4bd95adc9aeed16eed38b723
fadi <fadi.m.marouf@gmail.com>
2023-10-27 23:10:35 +0200

    set classes

--- src/pages/ShopCategory.jsx
+++ src/pages/ShopCategory.jsx
@@ -1,19 +1,19 @@
 import React, { useContext } from "react";
+import "./Css/ShopCategory.css";
 import { ShopContext } from "../Context/ShopContext";
 import all_products from "../components/assest/images/all_product";
 import Item from "../components/Items/Item";
 function ShopCategory(props) {
-    // const { all_products } = useContext(ShopContext);
     const { specialOffer, category } = props;
     const filteredItems = all_products
         .filter((item) => {
-            if (item.category === category) return item 
-            else return null
+            if (item.category === category) return item;
+            else return null;
         })
         .map((item) => <Item item={item} />);
     console.log({ filteredItems });
     return (
-        <div>
+        <div className="container">
             <div>
                 <img src={specialOffer} alt="" />
             </div>
@@ -21,7 +21,9 @@ function ShopCategory(props) {
                 {" "}
                 Showing 1 - {filteredItems.length} of {all_products.length}{" "}
             </p>
-            <div className="filtered-items"> {filteredItems} </div>
+            <div className="filtered">
+                <div className="filtered-items"> {filteredItems} </div>
+            </div>
         </div>
     );
 }
