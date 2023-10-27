3906eb5ecf373c378e0642052d2f455666d95918
fadi <fadi.m.marouf@gmail.com>
2023-10-27 15:12:39 +0200

    add specialOffer img

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Cart from "./pages/Cart";
 import Product from "./pages/Product";
 import ShopCategory from "./pages/ShopCategory";
 import Footer from "./components/Footer/Footer";
+import specialOffer_men from "./components/assest/images/banner_mens.png"
+import specialOffer_women from "./components/assest/images/banner_women.png"
+import specialOffer_kids from "./components/assest/images/banner_kids.png"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
     return (
@@ -17,14 +20,17 @@ function App() {
                     <Route
                         path="/men"
                         element={
-                            <ShopCategory specialOffer="men" category="men" />
+                            <ShopCategory
+                                specialOffer={specialOffer_men}
+                                category="men"
+                            />
                         }
                     />
                     <Route
                         path="/women"
                         element={
                             <ShopCategory
-                                specialOffer="women"
+                                specialOffer={specialOffer_women}
                                 category="women"
                             />
                         }
@@ -32,7 +38,10 @@ function App() {
                     <Route
                         path="/kids"
                         element={
-                            <ShopCategory specialOffer="kids" category="kids" />
+                            <ShopCategory
+                                specialOffer={specialOffer_kids}
+                                category="kids"
+                            />
                         }
                     />
                     <Route path="product" element={<Product />}>
