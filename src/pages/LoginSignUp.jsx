9d9c1558ce0244ccae774a709b018b9031c63c34
fadi <fadi.m.marouf@gmail.com>
2023-10-28 11:07:29 +0200

    login component

--- src/pages/LoginSignUp.jsx
+++ src/pages/LoginSignUp.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import "./Css/Login.css";
 
 function LoginSignUp() {
-  return (
-    <div>LoginSignUp</div>
-  )
+    const login = () => {};
+    return (
+        <div className="login">
+            <div className="login-container">
+
+                <h1>Login</h1>
+                <div className="login-fields">
+                    <input type="text" placeholder="Email address" />
+                    <input type="text" placeholder="Password" />
+                </div>
+                <button className="login-btn" onClick={login}> Continue </button>
+                <div className="createAccount">
+                    <p>Create an account ?</p>
+                    <a href="#"> sign up</a>
+                </div>
+                <input type="checkbox" name=" " id="" />
+                <p>By continuing I agree on the use terms and policy</p>
+            </div>
+        </div>
+    );
 }
 
-export default LoginSignUp
+export default LoginSignUp;
