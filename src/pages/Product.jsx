99b7c3771e31e0aaf0f75d1e1a5f926c59a9ffec
fadi <fadi.m.marouf@gmail.com>
2023-10-30 20:21:28 +0100

    add the location of the product

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,13 +1,23 @@
 import {useParams} from 'react-router-dom'
 import DisplayProduct from "../components/DisplayProduct/DisplayProduct";
+import all_products from "../components/assest/images/all_product"
+
+import breadcrum_arrow from '../components/assest/images/breadcrum_arrow.png'
 function Product() {
   const {productId}= useParams()
   console.log(useParams())
-  
+  const product = all_products.filter((item) => item.id === Number(productId));
+  console.log(product)
   
   return (
       <div>
-          <DisplayProduct productId={productId}/>
+          <div className="location">
+              HOME <img src={breadcrum_arrow} /> SHOP{" "}
+              <img src={breadcrum_arrow} />{" "}
+              {product[0].category.toLocaleUpperCase()}{" "}
+              <img src={breadcrum_arrow} /> {product[0].name}
+          </div>
+          <DisplayProduct productId={productId} />
       </div>
   );
 }
