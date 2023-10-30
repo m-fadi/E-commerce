b230fb30ecdb337d16b91e9750f0fc9b66a7061e
fadi <fadi.m.marouf@gmail.com>
2023-10-30 12:27:48 +0100

    add product component

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-
+import {useParams} from 'react-router-dom'
+import all_products from '../components/assest/images/all_product'
 function Product() {
+  const {productId}= useParams()
+  
+  const product=all_products.filter(item => item.id === Number(productId))
+  console.log(product[0])
   return (
-    <div>Product</div>
+    <div>
+      <img src={product[0].image} alt="" />
+    </div>
   )
 }
 
