f77db10e34e5c89f35c41f3b50d75092ad0f1178
fadi <fadi.m.marouf@gmail.com>
2023-10-30 17:45:45 +0100

    DisplayProduct comp

---
+++ src/components/DisplayProduct/DisplayProduct.jsx
@@ -0,0 +1,14 @@
+import all_products from "../assest/images/all_product";
+
+function DisplayProduct(props) {
+    const product = all_products.filter(
+        (item) => item.id === Number(props.productId)
+    );
+    return (
+        <div>
+            <img src={product[0].image} alt="" />;
+        </div>
+    );
+}
+
+export default DisplayProduct;
