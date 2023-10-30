659ccac08f6c88b00c0b1a9e42f8aa3cf8218e66
fadi <fadi.m.marouf@gmail.com>
2023-10-30 21:04:21 +0100

    add löinks to the location

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,25 +1,39 @@
-import {useParams} from 'react-router-dom'
+import "./Css/Product.css";
+import { useParams } from "react-router-dom";
 import DisplayProduct from "../components/DisplayProduct/DisplayProduct";
-import all_products from "../components/assest/images/all_product"
+import all_products from "../components/assest/images/all_product";
+import { Link } from "react-router-dom";
 
-import breadcrum_arrow from '../components/assest/images/breadcrum_arrow.png'
+import breadcrum_arrow from "../components/assest/images/breadcrum_arrow.png";
 function Product() {
-  const {productId}= useParams()
-  console.log(useParams())
-  const product = all_products.filter((item) => item.id === Number(productId));
-  console.log(product)
-  
-  return (
-      <div>
-          <div className="location">
-              HOME <img src={breadcrum_arrow} /> SHOP{" "}
-              <img src={breadcrum_arrow} />{" "}
-              {product[0].category.toLocaleUpperCase()}{" "}
-              <img src={breadcrum_arrow} /> {product[0].name}
-          </div>
-          <DisplayProduct productId={productId} />
-      </div>
-  );
+    const { productId } = useParams();
+    console.log(useParams());
+    const product = all_products.filter(
+        (item) => item.id === Number(productId)
+    );
+    console.log(product);
+
+    return (
+        <div>
+            <div className="location">
+                HOME <img src={breadcrum_arrow} />{" "}
+                <Link style={{ textDecoration: "none" }} to="/">
+                    {" "}
+                    Shop
+                </Link>{" "}
+                <img src={breadcrum_arrow} />{" "}
+                <Link
+                    style={{ textDecoration: "none" }}
+                    to={`/${product[0].category}`}
+                >
+                    {" "}
+                    {product[0].category.toLocaleUpperCase()}
+                </Link>{" "}
+                <img src={breadcrum_arrow} /> {product[0].name}
+            </div>
+            <DisplayProduct productId={productId} />
+        </div>
+    );
 }
 
-export default Product
+export default Product;
