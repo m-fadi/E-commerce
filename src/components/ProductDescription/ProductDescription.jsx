31804cd8c284390f35e0b602ebb2f2e7c3fa742f
fadi <fadi.m.marouf@gmail.com>
2023-10-31 15:43:47 +0100

    product description componant

--- src/components/ProductDescription/ProductDescription.jsx
+++ src/components/ProductDescription/ProductDescription.jsx
@@ -1,10 +1,33 @@
-import './ProductDescription.css'
-import React from 'react'
+import "./ProductDescription.css";
+import React from "react";
 
 function ProductDescription() {
-  return (
-    <div>ProductDescription</div>
-  )
+    return (
+        <div className="description-container">
+            <div className="descriptionBox-navigator">
+                <div className="description"> Description</div>
+                <div className="description reviews"> Reviews (89)</div>
+            </div>
+            <div className="about">
+                <p>
+                    {" "}
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Sunt quidem doloribus placeat explicabo eveniet eaque nihil
+                    magnam? Cum alias autem accusamus adipisci rem repudiandae
+                    qui corrupti nulla facilis? Libero earum dolorem placeat
+                    ullam, deleniti a aliquid debitis eveniet animi vitae esse?
+                    Quasi eveniet iste eius iusto reiciendis adipisci id
+                    similique.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Nesciunt explicabo quam corrupti facere maiores pariatur,
+                    eaque ullam sapiente qui neque vero nobis, alias sint.
+                    Molestias harum sequi eum voluptatum totam.
+                </p>
+            </div>
+        </div>
+    );
 }
 
-export default ProductDescription
+export default ProductDescription;
