6b54f721e1fcc8a58c62d982070c2675a5c26908
fadi <fadi.m.marouf@gmail.com>
2023-11-02 12:33:53 +0100

    remfromcart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
-
+import React,{useContext} from 'react'
+import { ShopContext } from '../Context/ShopContext'
+import Item from '../components/Items/Item'
 function Cart() {
+  const {cartItems, all_products, remFromCart} = useContext(ShopContext)
+  let items=all_products.map(item => {
+    if (cartItems[item.id] === 0) return null;
+    else return <Item item={item} key={item.id} />;
+
+    
+  })
+  let sum = items.reduce((prev,curr) =>prev+curr);
   return (
-    <div>Cart</div>
+    <div>
+     {sum===0 && <p className="empty-cart">There is no Items in the Cart</p>}
+    <div>{items}</div>
+    </div>
   )
 }
 
