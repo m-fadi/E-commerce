626003681f02ec73c0baf10d8b16eb31a5c0f725
fadi <fadi.m.marouf@gmail.com>
2023-11-02 12:33:33 +0100

    c

--- src/components/RelatedProducts/RelatedProducts.jsx
+++ src/components/RelatedProducts/RelatedProducts.jsx
@@ -1,23 +1,23 @@
 import "./RelatedProducts.css";
-import data from '../assest/images/data'
-import Item from '../Items/Item'
+import data from "../assest/images/data";
+import Item from "../Items/Item";
 
 function RelatedProducts() {
-    const relatedData=data.map(item=>{
+    const relatedData = data.map((item) => {
         return (
             <div>
                 {" "}
                 <Item key={item.id} item={item} />
             </div>
         );
-    })
-  return (
-      <div className="related-products-container">
-          <h1> RELATED PRODUCTS</h1>
-          <hr />
-          <div className="related-products"> {relatedData}</div>
-      </div>
-  );
+    });
+    return (
+        <div className="related-products-container">
+            <h1> RELATED PRODUCTS</h1>
+            <hr />
+            <div className="related-products"> {relatedData}</div>
+        </div>
+    );
 }
 
-export default RelatedProducts
+export default RelatedProducts;
