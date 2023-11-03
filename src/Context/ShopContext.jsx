4705e270603ebd347af57ca148447a02646956aa
fadi <fadi.m.marouf@gmail.com>
2023-11-03 12:13:24 +0100

    reform

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -14,6 +14,7 @@ const ShopContextProvider = (props) => {
     const [cartItems, setCartItems] = useState(getDefaultCart());
     const addToCart = (itemId) =>{
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))};
+    
     const remFromCart = (itemId) =>
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
     const contextValue = { all_products, cartItems, addToCart,remFromCart };
