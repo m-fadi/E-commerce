0a79a3ec291c6462ff61bb8de720d3679ee393a7
fadi <fadi.m.marouf@gmail.com>
2023-11-03 12:10:36 +0100

    modify buttons

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,43 +1,50 @@
-import React,{useContext} from 'react'
-import { ShopContext } from '../Context/ShopContext'
-import remove_icon from '../components/assest/images/cart_cross_icon.png'
+import React, { useContext } from "react";
+import { ShopContext } from "../Context/ShopContext";
+import remove_icon from "../components/assest/images/cart_cross_icon.png";
 
 function Cart() {
-  const {cartItems, all_products, remFromCart, addToCart} = useContext(ShopContext)
-  let items=all_products.map(item => {
-    if (cartItems[item.id] === 0) return null;
-    else return (
-        <div>            <div className="cart-items-format">
-                <img src={item.image} alt="" />
-                <p> {item.name}</p>
-                <p> price: {item.new_price} $</p>
-                <p> {cartItems[item.id]}</p>
-                <button
-                    onClick={() => addToCart(item.id)}
-                    className="cratItems-quantity"
-                > add item</button>
-                <button
-                    onClick={() => remFromCart(item.id)}
-                    className="cratItems-quantity"
-                >
-                    remove item
-                </button>
-                <p></p>
-                <img src={remove_icon} alt="" />
-            </div>
-            <hr />
+    const { cartItems, all_products, remFromCart, addToCart } =
+        useContext(ShopContext);
+    let items = all_products.map((item) => {
+        if (cartItems[item.id] === 0) return null;
+        else
+            return (
+                <div>
+                    {" "}
+                    <div className="cart-items-format">
+                        <img src={item.image} alt="" />
+                        <p> {item.name}</p>
+                        <p> price: {item.new_price} $</p>
+                        <p> {cartItems[item.id]}</p>
+                        <button
+                            onClick={() => addToCart(item.id)}
+                            className="cratItems-quantity"
+                        >
+                            {" "}
+                            add item
+                        </button>
+                        <button
+                            onClick={() => remFromCart(item.id)}
+                            className="cratItems-quantity"
+                        >
+                            remove item
+                        </button>
+                        <p></p>
+                        <img src={remove_icon} alt="" />
+                    </div>
+                    <hr />
+                </div>
+            );
+    });
+    let sum = items.reduce((prev, curr) => prev + curr);
+    return (
+        <div>
+            {sum === 0 && (
+                <p className="empty-cart">There is no Items in the Cart</p>
+            )}
+            <div>{items}</div>
         </div>
     );
-
-    
-  })
-  let sum = items.reduce((prev,curr) =>prev+curr);
-  return (
-    <div>
-     {sum===0 && <p className="empty-cart">There is no Items in the Cart</p>}
-    <div>{items}</div>
-    </div>
-  )
 }
 
-export default Cart
+export default Cart;
