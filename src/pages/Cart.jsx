27d5a436d43bf4180c5516d9d134a30ae9821802
fadi <fadi.m.marouf@gmail.com>
2023-11-04 12:46:26 +0100

    cart counter

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -5,7 +5,9 @@ import remove_icon from "../components/assest/images/cart_cross_icon.png";
 function Cart() {
     const { cartItems, all_products, remFromCart, addToCart } =
         useContext(ShopContext);
+       
     let items = all_products.map((item) => {
+     
         if (cartItems[item.id] === 0) return null;
         else
             return (
@@ -18,32 +20,45 @@ function Cart() {
                         <p> {cartItems[item.id]}</p>
                         <button
                             onClick={() => addToCart(item.id)}
-                            className="cratItems-quantity"
+                            
                         >
                             {" "}
                             add item
                         </button>
                         <button
                             onClick={() => remFromCart(item.id)}
-                            className="cratItems-quantity"
+                           
                         >
                             remove item
                         </button>
-                        <p></p>
+                        {/* <input
+                            type="number"
+                            min="0"
+                            className="form-control"
+                            id="amountInput"
+                            
+                            
+                        ></input> */}
+                        <p className="cratItems-quantity">
+                            {" "}
+                            {item.new_price * cartItems[item.id]}
+                        </p>
                         <img src={remove_icon} alt="" />
                     </div>
                     <hr />
                 </div>
             );
     });
-    let sum = items.reduce((prev, curr) => prev + curr);
+
+    // //let sum = items.reduce((prev, curr) => prev + curr);
     return (
         <div>
-            {sum === 0 && (
+            {/* {sum === 0 && (
                 <p className="empty-cart">There is no Items in the Cart</p>
-            )}
+            )} */}
             <div>{items}</div>
         </div>
+
     );
 }
 
