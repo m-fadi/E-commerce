807f2facfd7a3453b82754478bb182174536f78e
fadi <fadi.m.marouf@gmail.com>
2023-11-04 12:46:17 +0100

    fix bug

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -2,18 +2,22 @@ import React, { createContext, useState } from "react";
 import all_products from "../components/assest/images/all_product";
 
 export const ShopContext = createContext(null);
+
 const getDefaultCart = () => {
     let cart = {};
     for (let index = 0; index < all_products.length + 1; index++) {
         cart[index] = 0;
     }
+    console.log(cart)
     return cart;
 };
 const ShopContextProvider = (props) => {
     
     const [cartItems, setCartItems] = useState(getDefaultCart());
     const addToCart = (itemId) =>{
-    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))};
+      
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    }
     
     const remFromCart = (itemId) =>
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
