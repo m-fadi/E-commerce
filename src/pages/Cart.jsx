d9005aff07e2fb109ddd95ee94650f6449d018ea
fadi <fadi.m.marouf@gmail.com>
2023-11-06 14:47:11 +0100

    clean

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,64 +1,19 @@
 import React, { useContext } from "react";
+import "./Css/Cart.css";
 import { ShopContext } from "../Context/ShopContext";
-import remove_icon from "../components/assest/images/cart_cross_icon.png";
+
+import CartItem from "../components/CartItem/CartItem";
 
 function Cart() {
-    const { cartItems, all_products, remFromCart, addToCart } =
-        useContext(ShopContext);
-       
-    let items = all_products.map((item) => {
-     
-        if (cartItems[item.id] === 0) return null;
-        else
-            return (
-                <div>
-                    {" "}
-                    <div className="cart-items-format">
-                        <img src={item.image} alt="" />
-                        <p> {item.name}</p>
-                        <p> price: {item.new_price} $</p>
-                        <p> {cartItems[item.id]}</p>
-                        <button
-                            onClick={() => addToCart(item.id)}
-                            
-                        >
-                            {" "}
-                            add item
-                        </button>
-                        <button
-                            onClick={() => remFromCart(item.id)}
-                           
-                        >
-                            remove item
-                        </button>
-                        {/* <input
-                            type="number"
-                            min="0"
-                            className="form-control"
-                            id="amountInput"
-                            
-                            
-                        ></input> */}
-                        <p className="cratItems-quantity">
-                            {" "}
-                            {item.new_price * cartItems[item.id]}
-                        </p>
-                        <img src={remove_icon} alt="" />
-                    </div>
-                    <hr />
-                </div>
-            );
-    });
+    const { cartItems, all_products } = useContext(ShopContext);
 
-   let sum = items.reduce((prev, curr) => prev + curr);
+   
+
+    
     return (
         <div>
-            {sum === 0 && (
-                <p className="empty-cart">There is no Items in the Cart</p>
-            )}
-            <div>{items}</div>
+            <CartItem/>
         </div>
-
     );
 }
 
