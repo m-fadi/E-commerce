8c6a9bcc5dfc35efa73fde873607cea85ed806af
fadi <fadi.m.marouf@gmail.com>
2023-11-06 23:42:13 +0100

    checkout

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,26 +1,30 @@
-import React, { useContext, useEffect,useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../../Context/ShopContext";
 import "./CartItem.css";
 //import remove_icon from "../";
 
 function CartItem() {
+    let itemsPriceArray = [0];
     const { cartItems, all_products, removeProduct, addToCart } =
         useContext(ShopContext);
-        const [inputValue, setInputValue] = useState(0);
+    const [inputValue, setInputValue] = useState(0);
     const handleChange = (id, e) => {
-        e===0?  addToCart(id, 1):
-        addToCart(id,Number( e.target.value));
+        e === 0 ? addToCart(id, 1) : addToCart(id, Number(e.target.value));
     };
-    useEffect(()=>{
+    useEffect(() => {
         all_products.map((item) => {
-            if (cartItems[item.id] !== 0)
-             handleChange(item.id, 0)
-        })
-},[]) 
+            if (cartItems[item.id] !== 0) handleChange(item.id, 0);
+        });
+    }, []);
 
     let items = all_products.map((item) => {
         if (cartItems[item.id] === 0) return null;
-        else
+        else {
+            itemsPriceArray = [
+                ...itemsPriceArray,
+                item.new_price * cartItems[item.id],
+            ];
+
             return (
                 <div>
                     <div className="cart-items-format">
@@ -35,40 +39,58 @@ function CartItem() {
                                 total : {item.new_price * cartItems[item.id]} $
                             </div>
                         </div>
-                        
+
                         <input
                             type="number"
                             min="1"
                             max="100"
                             onChange={(e) => handleChange(item.id, e)}
                         ></input>
-                        <button className="removeProduct-btn" onClick={() => removeProduct(item.id)}>
+                        <button
+                            className="removeProduct-btn"
+                            onClick={() => removeProduct(item.id)}
+                        >
                             {" "}
                             Remove product
                         </button>
-
-                       
                     </div>
                 </div>
             );
+        }
     });
     let sum = items.reduce((prev, curr) => prev + curr);
     console.log({ items });
     return (
         <div className="cart-container">
-            <p className="cart-title"> Shopping Cart </p>
-            <hr />
-            {sum === 0 && (
-                <div>
-                    <p className="empty-cart">Your SHOPER Cart is empty.</p>
+            <div className="productAndPrices">
+                <p className="cart-title"> Shopping Cart </p>
+                <hr />
+                {sum === 0 && (
+                    <div>
+                        <p className="empty-cart">Your SHOPER Cart is empty.</p>
+                    </div>
+                )}
+
+                <div>{items}</div>
+                <hr />
+                <div className="cart-total">
+                    <p>
+                        {" "}
+                        Total Price:{" "}
+                        {itemsPriceArray.reduce((prev, curr) => prev + curr)} $
+                    </p>
+                </div>
+            </div>
+            {itemsPriceArray.length > 1 && (
+                <div className="cart-checkOut">
+                    <p>
+                        {" "}
+                        Total Price:{" "}
+                        {itemsPriceArray.reduce((prev, curr) => prev + curr)} $
+                    </p>
+                    <button className="cart-checkOut-btn"> Check Out</button>
                 </div>
             )}
-
-            <div>{items}</div>
-            <hr/>
-             <div className="cart-total">
-                            <p> Total Price: 0 $</p>
-                        </div>
         </div>
     );
 }
