14eb2246d7121a2763451806320b2c03b672886a
fadi <fadi.m.marouf@gmail.com>
2023-11-08 02:59:09 +0100

    clean up

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,21 +1,17 @@
 import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../../Context/ShopContext";
 import "./CartItem.css";
-//import remove_icon from "../";
+import remove_icon from "../assest/images/cart_cross_icon.png";
 
 function CartItem() {
     let itemsPriceArray = [0];
     const { cartItems, all_products, removeProduct, addToCart } =
         useContext(ShopContext);
-    
+
     const handleChange = (id, e) => {
         e === 0 ? addToCart(id, 1) : addToCart(id, Number(e.target.value));
     };
-    useEffect(() => {
-        all_products.map((item) => {
-            if (cartItems[item.id] !== 0) handleChange(item.id, 0);
-        });
-    }, []);
+    
 
     let items = all_products.map((item) => {
         if (cartItems[item.id] === 0) return null;
@@ -24,74 +20,109 @@ function CartItem() {
                 ...itemsPriceArray,
                 item.new_price * cartItems[item.id],
             ];
-
             return (
-                <div>
-                    <div className="cart-items-format">
-                        <div className="cart-item-details">
-                            {" "}
-                            <img src={item.image} alt="" />
-                            <div className="qus">
-                                <p> {item.name}</p>
-                                <p className="cratItems-quantity">
-                                    price per Stk. {item.new_price} $
-                                </p>
-                                total : {item.new_price * cartItems[item.id]} $
-                            </div>
-                        </div>
-
-                        <input
-                            type="number"
-                            min="1"
-                           
-                            max="100"
-                            onChange={(e) => handleChange(item.id, e)}
-                        ></input>
-
-                        <button
-                            className="removeProduct-btn"
-                            onClick={() => removeProduct(item.id)}
-                        >
-                            {" "}
-                            Remove product
-                        </button>
-                    </div>
+                <div className="cartItems-format">
+                    <img src={item.image} alt="" className="cartIcon-product--icon" />
+                    <p> {item.name}</p>
+                    <p>{item.new_price} $</p>
+                    <input
+                        type="number"
+                        min="1"
+                        max="100"
+                        onChange={(e) => handleChange(item.id, e)}
+                    ></input>
+                    <p>
+                        Total Price:{" "}
+                        {itemsPriceArray.reduce((prev, curr) => prev + curr)} ${" "}
+                    </p>
+                    <img
+                        src={remove_icon}
+                        onClick={() => removeProduct(item.id)}
+                        alt=""
+                    />
                 </div>
             );
+            // return (
+            //     <div>
+            //         <div className="cart-items-format">
+            //             <div className="cart-item-details">
+            //                 {" "}
+            //                 <img src={item.image} alt="" />
+            //                 <div className="qus">
+            //                     <p> {item.name}</p>
+            //                     <p className="cratItems-quantity">
+            //                         price per Stk. {item.new_price} $
+            //                     </p>
+            //                     total : {item.new_price * cartItems[item.id]} $
+            //                 </div>
+            //             </div>
+
+            //             <input
+            //                 type="number"
+            //                 min="1"
+
+            //                 max="100"
+            //                 onChange={(e) => handleChange(item.id, e)}
+            //             ></input>
+
+            //             <button
+            //                 className="removeProduct-btn"
+            //                 onClick={() => removeProduct(item.id)}
+            //             >
+            //                 {" "}
+            //                 Remove product
+            //             </button>
+            //         </div>
+            //     </div>
+            // );
         }
     });
     let sum = items.reduce((prev, curr) => prev + curr);
     console.log({ items });
     return (
-        <div className="cart-container">
-            <div className="productAndPrices">
-                <p className="cart-title"> Shopping Cart </p>
-                <hr />
-                {sum === 0 && (
-                    <div>
-                        <p className="empty-cart">Your SHOPER Cart is empty.</p>
-                    </div>
-                )}
-                <div>{items}</div>
-                <hr />
-                <div className="cart-total">
-                    <p>
-                        {" "}
-                        Total Price:{" "}
-                        {itemsPriceArray.reduce((prev, curr) => prev + curr)} $
-                    </p>
-                </div>
+        // <div className="cart-container">
+        //     <div className="productAndPrices">
+        //         <p className="cart-title"> Shopping Cart </p>
+        //         <hr />
+        //         {sum === 0 && (
+        //             <div>
+        //                 <p className="empty-cart">Your SHOPER Cart is empty.</p>
+        //             </div>
+        //         )}
+        //         <div>{items}</div>
+        //         <hr />
+        //         <div className="cart-total">
+        //             <p>
+        //                 {" "}
+        //                 Total Price:{" "}
+        //                 {itemsPriceArray.reduce((prev, curr) => prev + curr)} $
+        //             </p>
+        //         </div>
+        //     </div>
+        //     {itemsPriceArray.length > 1 && (
+        //         <div className="cart-checkOut">
+        //             <p>
+        //                 {" "}
+        //                 Total Price:{" "}
+        //                 {itemsPriceArray.reduce((prev, curr) => prev + curr)} $
+        //             </p>
+        //             <button className="cart-checkOut-btn"> Check Out</button>
+        //         </div>
+        //     )}
+        // </div>
+        <div className="cartItems">
+            <div className="cartItems-format-main">
+                <p>Products</p>
+                <p>Title</p>
+                <p>Price</p>
+                <p>Qty</p>
+                <p>Total</p>
+                <p>Remove</p>
             </div>
-            {itemsPriceArray.length > 1 && (
-                <div className="cart-checkOut">
-                    <p>
-                        {" "}
-                        Total Price:{" "}
-                        {itemsPriceArray.reduce((prev, curr) => prev + curr)} $
-                    </p>
-                    <button className="cart-checkOut-btn"> Check Out</button>
-                </div>
-            )}
+            <hr />
+            <div>
+                {items}
+            </div>
         </div>
     );
 }
