ca5a7293cad228272b7de6e9b50d0cacec596667
fadi <fadi.m.marouf@gmail.com>
2023-11-08 04:37:25 +0100

    addd

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ShopContextProvider from "./Context/ShopContext"
-
+if (module.hot && process.env.NODE_ENV !== "production") {
+    module.hot.accept();
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ShopContextProvider>
